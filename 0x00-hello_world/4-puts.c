de7ccfc1f9a13d29ae606b9b7f33e98505ab47c0
Naddy-Code <mohammednadia239@gmail.com>
2023-06-15 15:52:46 -0700

    Hello,printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A progam that prints a line with put function
+ * Return: 0 
+ */
+int main(void)
+{
+puts("\"Progamming is like building a multilingual puzzle");
+return (0);
+}
