a29b9f4585088d1464f1a99e53c1a0d361f81952
Naddy-Code <mohammednadia239@gmail.com>
2023-06-17 03:12:19 -0700

    A program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
  * main - A program that prints the alphabet in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-int lowerCase = "a";
-while (lowerCase <= "z")
-{
-putchar(lowerCase);
-lowerCase += 1;
-}
-putchar("\n");
-return (0);
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
 }
