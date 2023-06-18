6d561aaf6f1ecd080ff0d24c7a587fdf60c902b2
Naddy-Code <mohammednadia239@gmail.com>
2023-06-18 15:27:09 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A progam that prints a line with put function
- * Return: Always 0 (success)
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Progamming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
