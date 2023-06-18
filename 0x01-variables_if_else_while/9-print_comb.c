8f54cd1c04ee5c308256fd2e7cb458b550354409
Naddy-Code <mohammednadia239@gmail.com>
2023-06-18 13:20:30 -0700

    A program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - A program that prints all possible combinations of SD numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++) {
+		putchar(a + '0');
+	        if (a < 9) {
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
