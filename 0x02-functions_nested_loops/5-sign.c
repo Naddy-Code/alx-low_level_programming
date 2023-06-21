d1983396c1dc32f6d07bbb4c9d9bcd7f2f213178
Naddy-Code <mohammednadia239@gmail.com>
2023-06-21 13:16:02 -0700

    A function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@
  *
  * @n: takes integer type input for function
  *
- * Retun: 1 if +, 0 if 0 and -1 if -
+ * Retun: 1 if +, 0 if 0 -1 if - and / if not a digit
  */
 
 int print_sign(int n)
@@ -20,9 +20,14 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
+	}
+	else
+	{
+		_putchar(45);
+		return ('/');
 	}
 }
