72f36a71b606798f2c88999e2dc43e4fc00acdad
Naddy-Code <mohammednadia239@gmail.com>
2023-06-21 16:19:18 -0700

    A program that prints the first 50 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_to_98 - print all natural numbers from n to 98.
- * @n: the number to start counting from to 98
- * Return: void returns nothing.
+ * print_to_98 - function that prints all natural numbers from n to 98
+ *
+ * @n: input
  */
-void print_to_98(int n)
+
+void ptint_to_98(int n)
 {
-	if (n < 98)
-	{
-		for (n = n; n < 98; n++)
-			printf("%d, ", n);
-		printf("%d\n", 98);
-	}
-	else
-	{
-		for (n = n; n > 98; n--)
-			printf("%d, ", n);
-		printf("%d\n", 98);
-	}
-}
+	int count;
+
+	if (n > 98)
+		for (coun
