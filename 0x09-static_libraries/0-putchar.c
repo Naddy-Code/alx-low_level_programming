fe12225a3f80bb4c20381a91c0fb9d2e5db16f34
Naddy-Code <mohammednadia239@gmail.com>
2023-07-07 10:27:18 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,18 @@
+
+/**
+ * main - program that prints putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
